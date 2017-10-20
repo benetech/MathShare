b5ead95a82762fd2bfc39eb0e76fc7ea9bd20855
Jason Schwab <jasons@benetech.org>
2017-10-19 23:59:51 -0700

    Added several examples

--- data/data01.js
+++ data/data01.js
@@ -1,3 +1,4 @@
+/*
 var problem01 =    [{"metadata": [
 							{
 							"title":"Problem 1",
@@ -38,7 +39,33 @@ var problem01 =    [{"metadata": [
 		    				"annotation":"Divide both sides by 7. 7/7=1 and 1 times anything is just that thing, so we just have x. On the right hand side of the equation, we have 14/7. That is = 2. Clean up the cross outs and..."
 		    				}]
 	    			}];
+*/
 	    			
+var problem01 =    [{"metadata": [
+							{
+							"title":"Problem 1",
+		    				"variableName":"problem01"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"3(-1/6)(-2/5)",
+		    				"annotation":"Find the product"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"3(-1/6)(-2/5)",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"3(-1/6)(-2/5)",
+		    				"annotation":"Find the product"	
+		    				}]
+	    			}];	
+
 var problem02 =    [{"metadata": [
 							{
 							"title":"Problem 2",
@@ -47,30 +74,152 @@ var problem02 =    [{"metadata": [
     			    },
 					{"originalProblem": [
 							{
-		    				"equation":"$$222x + 13 = -1$$",
+		    				"equation":"7x-13 = 1",
 		    				"annotation":"Solve for x"
 		    				}]
     			    },
     			    {"currentEditor": [
 							{
-		    				"equation":"222x + 13 = -1",
-		    				"annotation":"Solve for x"
+		    				"equation":"7x-13 = 1",
+		    				"annotation":""
 		    				}]
 	    			},
 	    			{"history": [
 							{
-		    				"equation":"222x + 13 = -1",
+		    				"equation":"7x-13 = 1",
 		    				"annotation":"Solve for x"
-		    				},
-		    				{
-		    				"equation":"222x = -2",
-		    				"annotation":"Final answer. You can delete this and previous steps and re-derive to play with Galois."
+		    				}]
+	    			}];	   
+
+var problem03 =    [{"metadata": [
+							{
+							"title":"Problem 3",
+		    				"variableName":"problem03"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"3x+14 = 5",
+		    				"annotation":"Solve for x"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"3x+14 = 5",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"3x+14 = 5",
+		    				"annotation":"Solve for x"
+		    				}]
+	    			}];
+	    			
+var problem04 =    [{"metadata": [
+							{
+							"title":"Problem 4",
+		    				"variableName":"problem04"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"b/9-34 < -36",
+		    				"annotation":"Solve the inequality"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"b/9-34 < -36",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"b/9-34 < -36",
+		    				"annotation":"Solve the inequality"
 		    				}]
 	    			}];	    			
+	    			
+var problem05 =    [{"metadata": [
+							{
+							"title":"Problem 5",
+		    				"variableName":"problem05"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"55555",
+		    				"annotation":"55555"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"55555",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"55555",
+		    				"annotation":"55555"
+		    				}]
+	    			}];	    			
+	    			
+var problem06 =    [{"metadata": [
+							{
+							"title":"Problem 6",
+		    				"variableName":"problem06"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"66666",
+		    				"annotation":"66666"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"66666",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"66666",
+		    				"annotation":"66666"
+		    				}]
+	    			}];	    			
+	    			 			
+var problem07 =    [{"metadata": [
+							{
+							"title":"Problem 7",
+		    				"variableName":"problem07"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"77777",
+		    				"annotation":"77777"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"77777",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"77777",
+		    				"annotation":"77777"
+		    				}]
+	    			}];
+
 
 var newEditor =    [{"metadata": [
 							{
-							"title":"Blank Editor",
+							"title":"Try your own problem",
 		    				"variableName":"newEditor"
 		    				}]
     			    },
