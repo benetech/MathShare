22dad93c3599adfc95970f046b5a3fdb22a94b63
Neil Soiffer <soiffer@alum.mit.edu>
2017-10-20 00:20:32 -0700

    Restore accidentally deleted 'calc' JS. Fixes #39

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -294,6 +294,56 @@ function CleanUpCrossouts(latexStr) {
 }
 
 //***************************************************************************************************************************************************
+// Grab the selection, remove any cross outs, and then do any binary arithmetic operations.
+// If anything is done, cross out the selection and add the calculated result after it.
+// Otherwise, alert that no calculations could be done.
+function CalculateAndReplace(element) {
+	
+	let doCalculation = function(latex) {
+		// Return either the calculated result (as a string) or an empty string if can't calculate
+		// Start by converting various character points into one set
+		latex = latex.replace(/\\times/g, '*')
+					 .replace(/\\cdot/g, '*')
+					 .replace(/\\div/g, '/')
+					 .replace(/\\frac{(.+?)}{(.+?)}/g, '($1)/($2)');
+		
+		// make sure there are numbers AND operators
+		if ( !(/[\d.]/.test(latex) && /[+\-*/]/.test(latex)) ) {
+			return "";
+		}
+		// avoid security issues, etc., and rule out letters, etc, that can be part of JS program
+		if ( /[a-zA-Z<=>]/.test(latex) ) {
+			return "";
+		}
+		
+		try {
+			return eval(latex);
+		} catch(e) {
+			return "";
+		}
+	}
+	if ( TheActiveMathField.selectionIsCollapsed() ) {
+		return alert( "You must select an arithmetic expression for calculation." );
+	}
+	
+	// if the insertionString contains a cross out, remove all crossout in the selection
+	let selection = TheActiveMathField.selectedText('latex')
+						.replace(CrossoutFindRegExpPattern, "");
+
+	let result = doCalculation(selection);
+	if (!result) {
+		return alert( "Selection must contain only numbers and operators.");
+	}
+	
+	let insertionString = CrossoutTeXString + "{" + selection + "}" + result;
+
+	TheActiveMathField.perform(['insert', insertionString, 
+				{insertionMode: 'replaceSelection',
+				 selectionMode: 'after'}]);
+	TheActiveMathField.focus();
+}
+
+//***************************************************************************************************************************************************
 // Paste the button into the active math editor after substituting
 // for the black and white squares
 function MathLivePasteFromButton(element) {
@@ -319,6 +369,7 @@ function MathLivePasteFromButton(element) {
 				 selectionMode: 'placeholder'}]);
 	TheActiveMathField.focus();
 }
+
 
 //***************************************************************************************************************************************************
 // Call paste function if someone hits enter over palette entry
