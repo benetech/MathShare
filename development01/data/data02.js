e4839e892e7b4ec1006ea5ed11d73b8ea1173dee
Jason Schwab <jasons@benetech.org>
2017-10-25 15:16:23 -0700

    Update the equations in dev
    https://github.com/benetech/MathEditor/issues/57

---
+++ development01/data/data02.js
@@ -0,0 +1,240 @@
+/*
+var problem01 =    [{"metadata": [
+							{
+							"title":"Problem 1",
+		    				"variableName":"problem01"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"$$7x + 13 = -1$$",
+		    				"annotation":"Solve for x"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"x = -2",
+		    				"annotation":"Final answer. You can delete this and previous steps and re-derive to play with Galois."
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"7x + 13 = -1",
+		    				"annotation":"Solve for x"
+		    				},
+		    				{
+		    				"equation":"7x + 13-13 = -1-13",
+		    				"annotation":"Subtract 13 from both sides"
+		    				},
+		    				{
+		    				"equation":"7x \\enclose{updiagonalstrike downdiagonalstrike}[2px solid red]{+ 13-13} = \\enclose{updiagonalstrike downdiagonalstrike}[2px solid red]{-1-13} -14",
+		    				"annotation":"Do arithmetic"
+		    				},
+		    				{
+		    				"equation":"7x =-14",
+		    				"annotation":""
+		    				},
+		    				{
+		    				"equation":'\\enclose{updiagonalstrike downdiagonalstrike}[2px solid red]{7}x =-\\enclose{updiagonalstrike downdiagonalstrike}[2px solid red]{14}2',
+		    				"annotation":"Divide both sides by 7. 7/7=1 and 1 times anything is just that thing, so we just have x. On the right hand side of the equation, we have 14/7. That is = 2. Clean up the cross outs and..."
+		    				}]
+	    			}];
+*/
+	    			
+var problem01 =    [{"metadata": [
+							{
+							"title":"Problem 1",
+		    				"variableName":"problem01"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"3(-\\frac{1}{6})(-\\frac{2}{5})=",
+		    				"annotation":"Find the product"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"3(-\\frac{1}{6})(-\\frac{2}{5})=",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"3(-\\frac{1}{6})(-\\frac{2}{5})=",
+		    				"annotation":"Find the product"	
+		    				}]
+	    			}];	
+
+var problem02 =    [{"metadata": [
+							{
+							"title":"Problem 2",
+		    				"variableName":"problem02"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"-\\frac{2}{5}(-\\frac{1}{2})(-\\frac{5}{6})=",
+		    				"annotation":"Find the product"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"-\\frac{2}{5}(-\\frac{1}{2})(-\\frac{5}{6})=",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"-\\frac{2}{5}(-\\frac{1}{2})(-\\frac{5}{6})=",
+		    				"annotation":"Find the product"
+		    				}]
+	    			}];	   
+
+var problem03 =    [{"metadata": [
+							{
+							"title":"Problem 3",
+		    				"variableName":"problem03"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"\\frac{55}{\\frac{1}{2}}=",
+		    				"annotation":"Find the quotient"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"\\frac{55}{\\frac{1}{2}}=",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"\\frac{55}{\\frac{1}{2}}=",
+		    				"annotation":"Find the quotient"
+		    				}]
+	    			}];
+	    			
+var problem04 =    [{"metadata": [
+							{
+							"title":"Problem 4",
+		    				"variableName":"problem04"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"\\frac{3}{10}\\div (\\frac{5}{8})=",
+		    				"annotation":"Find the quotient"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"\\frac{3}{10}\\div (\\frac{5}{8})=",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"\\frac{3}{10}\\div (\\frac{5}{8})=",
+		    				"annotation":"Find the quotient"
+		    				}]
+	    			}];	    			
+	    			
+var problem05 =    [{"metadata": [
+							{
+							"title":"Problem 5",
+		    				"variableName":"problem05"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"",
+		    				"annotation":"Sarah works at a coffee shop. Her weekly salary is $325 and she early 11.5% commission on sales. How much does she make if she sells $2800 in merchandise?"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"",
+		    				"annotation":"Sarah works at a coffee shop. Her weekly salary is $325 and she early 11.5% commission on sales. How much does she make if she sells $2800 in merchandise?"
+		    				}]
+	    			}];	    			
+	    			
+var problem06 =    [{"metadata": [
+							{
+							"title":"Problem 6",
+		    				"variableName":"problem06"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"7x-13=1",
+		    				"annotation":"Solve for x"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"7x-13=1",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"7x-13=1",
+		    				"annotation":"Solve for x"
+		    				}]
+	    			}];	    			
+	    			 			
+var problem07 =    [{"metadata": [
+							{
+							"title":"Problem 7",
+		    				"variableName":"problem07"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				"equation":"\\frac{b}{9}-34\\leq -36",
+		    				"annotation":"Solve the inequality"
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				"equation":"\\frac{b}{9}-34\\leq -36",
+		    				"annotation":""
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				"equation":"\\frac{b}{9}-34\\leq -36",
+		    				"annotation":"Solve the inequality"
+		    				}]
+	    			}];
+
+
+var newEditor =    [{"metadata": [
+							{
+							"title":"Try your own problem",
+		    				"variableName":"newEditor"
+		    				}]
+    			    },
+					{"originalProblem": [
+							{
+		    				
+		    				}]
+    			    },
+    			    {"currentEditor": [
+							{
+		    				
+		    				}]
+	    			},
+	    			{"history": [
+							{
+		    				
+		    				}]
+	    			}];		
