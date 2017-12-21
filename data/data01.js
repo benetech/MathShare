287b236c41ae7a7666f9e6be658a6bdae81d4a93
Jason Schwab <jasons@benetech.org>
2017-12-21 11:57:58 -0800

    trial

--- data/data01.js
+++ data/data01.js
@@ -1,197 +1,144 @@
-var problem01 =    [{"metadata": [
-							{
-							"title":"Problem 1",
-		    				"variableName":"problem01"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				"equation":"3(-\\frac{1}{6})(-\\frac{2}{5})",
-		    				"annotation":"Find the product"
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				"equation":"3(-\\frac{1}{6})(-\\frac{2}{5})",
-		    				"annotation":""
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				"equation":"3(-\\frac{1}{6})(-\\frac{2}{5})",
-		    				"annotation":"Find the product"	
-		    				}]
-	    			}];	
-
-var problem02 =    [{"metadata": [
-							{
-							"title":"Problem 2",
-		    				"variableName":"problem02"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				"equation":"7x-13 = 1",
-		    				"annotation":"Solve for x"
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				"equation":"7x-13 = 1",
-		    				"annotation":""
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				"equation":"7x-13 = 1",
-		    				"annotation":"Solve for x"
-		    				}]
-	    			}];	   
-
-var problem03 =    [{"metadata": [
-							{
-							"title":"Problem 3",
-		    				"variableName":"problem03"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				"equation":"3x+14 = 5",
-		    				"annotation":"Solve for x"
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				"equation":"3x+14 = 5",
-		    				"annotation":""
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				"equation":"3x+14 = 5",
-		    				"annotation":"Solve for x"
-		    				}]
-	    			}];
-	    			
-var problem04 =    [{"metadata": [
-							{
-							"title":"Problem 4",
-		    				"variableName":"problem04"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				"equation":"\\frac{b}{9}-34 \\leq -36",
-		    				"annotation":"Solve the inequality"
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				"equation":"\\frac{b}{9}-34 \\leq -36",
-		    				"annotation":""
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				"equation":"\\frac{b}{9}-34 \\leq -36",
-		    				"annotation":"Solve the inequality"
-		    				}]
-	    			}];	    			
-	    			
-var problem05 =    [{"metadata": [
-							{
-							"title":"Problem 5",
-		    				"variableName":"problem05"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				"equation":"",
-		    				"annotation":"Solve for <span class='staticMath'>$$x$$<span>: Angles <span class='staticMath'>$$a$$<span> and <span class='staticMath'>$$b$$<span> are supplementary. Angle <span class='staticMath'>$$a$$<span> is 78<span class='staticMath'>$$\\degree$$</span> and angle <span class='staticMath'>$$b$$<span> is equal to <span class='staticMath'>$$3x$$<span>"
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				"equation":"",
-		    				"annotation":""
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				"equation":"",
-		    				"annotation":"Solve for <span class='staticMath'>$$x$$<span>: Angles <span class='staticMath'>$$a$$<span> and <span class='staticMath'>$$b$$<span> are supplementary. Angle <span class='staticMath'>$$a$$<span> is 78<span class='staticMath'>$$\\degree$$</span> and angle <span class='staticMath'>$$b$$<span> is equal to <span class='staticMath'>$$3x$$<span>"
-		    				}]
-	    			}];	    			
-	    			
-var problem06 =    [{"metadata": [
-							{
-							"title":"Problem 6",
-		    				"variableName":"problem06"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				"equation":"",
-		    				"annotation":"What is the area of a circle with a diameter of 10 (use <span class='staticMath'>$$\\pi$$</span> or the value of <span class='staticMath'>$$\\pi$$</span> to the hundredths place)?"
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				"equation":"",
-		    				"annotation":""
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				"equation":"",
-		    				"annotation":"What is the area of a circle with a diameter of 10 (use <span class='staticMath'>$$\\pi$$</span> or the value of <span class='staticMath'>$$\\pi$$</span> to the hundredths place)?"
-		    				}]
-	    			}];	    			
-	    			 			
-var problem07 =    [{"metadata": [
-							{
-							"title":"Problem 7",
-		    				"variableName":"problem07"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				"equation":"",
-		    				"annotation":"Sarah works at a coffee shop. Her weekly salary is $325 and she early 11.5% commission on sales. How much does she make if she sells $2800 in merchandise?"
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				"equation":"",
-		    				"annotation":""
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				"equation":"",
-		    				"annotation":"Sarah works at a coffee shop. Her weekly salary is $325 and she early 11.5% commission on sales. How much does she make if she sells $2800 in merchandise?"
-		    				}]
-	    			}];
-
-
-var newEditor =    [{"metadata": [
-							{
-							"title":"Try your own problem",
-		    				"variableName":"newEditor"
-		    				}]
-    			    },
-					{"originalProblem": [
-							{
-		    				
-		    				}]
-    			    },
-    			    {"currentEditor": [
-							{
-		    				
-		    				}]
-	    			},
-	    			{"history": [
-							{
-		    				
-		    				}]
-	    			}];		
+{
+	"metadata": { "title": "Mixed Problems" },
+	"problems": [
+		{
+			"metadata": {
+				"title": "Problem 1"
+			},
+			"originalProblem": {
+				"equation": "7773(-\\frac{1}{6})(-\\frac{2}{5})",
+				"annotation": "777Find the product"
+			},
+			"currentEditor": {
+				"equation": "7773(-\\frac{1}{6})(-\\frac{2}{5})",
+				"annotation": ""
+			},
+			"history": [
+				{
+					"equation": "7773(-\\frac{1}{6})(-\\frac{2}{5})",
+					"annotation": "Find the product"
+				}
+			]
+		},
+		{
+			"metadata": {
+				"title": "Problem 2"
+			},
+			"originalProblem": {
+				"equation": "-\\frac{2}{5}(-\\frac{1}{2})(-\\frac{5}{6})",
+				"annotation": "Find the product"
+			},
+			"currentEditor": {
+				"equation": "-\\frac{2}{5}(-\\frac{1}{2})(-\\frac{5}{6})",
+				"annotation": ""
+			},
+			"history": [
+				{
+				"equation": "-\\frac{2}{5}(-\\frac{1}{2})(-\\frac{5}{6}})",
+					"annotation": "Find the product"
+				}
+			]
+		},
+		{
+			"metadata": {
+				"title": "Problem 3"
+			},
+			"originalProblem": {
+				"equation": "\\frac{55}{\\frac{1}{2}}",
+				"annotation": "Find the quotient"
+			},
+			"currentEditor": {
+				"equation": "\\frac{55}{\\frac{1}{2}}",
+				"annotation": ""
+			},
+			"history": [
+				{
+					"equation": "\\frac{55}{\\frac{1}{2}",
+					"annotation": "Find the quotient"
+				}
+			]
+		},
+		{
+			"metadata": {
+				"title": "Problem 4"
+			},
+			"originalProblem": {
+				"equation": "\\frac{3}{10}\\div (\\frac{5}{8})",
+				"annotation": "Find the quotient"
+			},
+			"currentEditor": {
+				"equation": "\\frac{3}{10}\\div (\\frac{5}{8})",
+				"annotation": ""
+			},
+			"history": [
+				{
+					"equation": "\\frac{3}{10}\\div (\\frac{5}{8})",
+					"annotation": "Find the quotient"
+				}
+			]
+		},
+		{
+			"metadata": {
+				"title": "Problem 5"
+			},
+			"originalProblem": {
+				"equation": "",
+				"annotation": "Sarah works at a coffee shop. Her weekly salary is $325 and she earns 11.5% commission on sales. How much does she make if she sells $2800 in merchandise?"
+			},
+			"currentEditor": {
+				"equation": "",
+				"annotation": ""
+			},
+			"history": [
+				{
+					"equation": "",
+					"annotation": "Sarah works at a coffee shop. Her weekly salary is $325 and she earns 11.5% commission on sales. How much does she make if she sells $2800 in merchandise?"
+				}
+			]
+		},
+		{
+			"metadata": {
+				"title": "Problem 6"
+			},
+			"originalProblem": {
+				"equation": "7x-13=1",
+				"annotation": "Solve for x"
+			},
+			"currentEditor": {
+				"equation": "7x-13=1",
+				"annotation": ""
+			},
+			"history": [
+				{
+					"equation": "7x-13=1",
+					"annotation": "Solve for x"
+				}
+			]
+		},
+		{
+			"metadata": {
+				"title": "Problem 7"
+			},
+			"originalProblem": {
+				"equation": "\\frac{b}{9}-34\\leq -36",
+				"annotation": "Solve the inequality"
+			},
+			"currentEditor": {
+				"equation": "\\frac{b}{9}-34\\leq -36",
+				"annotation": ""
+			},
+			"history": [
+				{
+					"equation": "\\frac{b}{9}-34\\leq -36",
+					"annotation": "Solve the inequality"
+				}
+			]
+		},
+		{
+			"metadata": {"title":"Try your own problem",    "variableName":"newEditor"},
+			"originalProblem": {"equation":"", "annotation":"Try your own problem"},
+			"currentEditor": {},
+			"history": [{}]
+		}
+	]
+}
