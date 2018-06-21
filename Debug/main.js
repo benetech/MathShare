1d016367565b396083e1b7ac64a46a13976efa3c
oskarhinc <ohinc@soldevelo.com>
2018-06-21 11:16:03 +0200

    Replaced tabs with two spaces

--- Debug/main.js
+++ Debug/main.js
@@ -1,27 +1,27 @@
 define(['mathlive/mathlive'], 
     function(mLive) {
-	  MathLive = mLive;
-	  MathLive.renderMathInDocument();
-	  TheActiveMathField = MathLive.makeMathField(
-		document.getElementById('mathEditorActive'),
-	  	{commandbarToggle: 'hidden',
-		 overrideDefaultInlineShortcuts: false,
-		 inlineShortcuts: { '>-': '>-',			// override builtin shortcut (\succ)
-							'<-': '<-',			// override builtin shortcut (\leftarrow)
-							'<=': '\\leq',		// use more familiar ≤
-							'>=': '\\geq',		// use more familar ≥
-							'$': '\\$',			// make it easy to type $
-							'%': '\\%',			// make it easy to type %
-							'*': '\\times',		// what most people want
-							'?=': '\\overset{?}{=}'	// is equal to
-						  }
+    MathLive = mLive;
+    MathLive.renderMathInDocument();
+    TheActiveMathField = MathLive.makeMathField(
+    document.getElementById('mathEditorActive'),
+      {commandbarToggle: 'hidden',
+     overrideDefaultInlineShortcuts: false,
+     inlineShortcuts: { '>-': '>-',			// override builtin shortcut (\succ)
+              '<-': '<-',			// override builtin shortcut (\leftarrow)
+              '<=': '\\leq',		// use more familiar ≤
+              '>=': '\\geq',		// use more familar ≥
+              '$': '\\$',			// make it easy to type $
+              '%': '\\%',			// make it easy to type %
+              '*': '\\times',		// what most people want
+              '?=': '\\overset{?}{=}'	// is equal to
+              }
          //onSelectionDidChange: UpdatePalette
-		}
-	  );
-	  document.onkeydown = HandleKeyDown;
-		$(document).ready(function() {
-			//generate all problem data for main page -- this is asynchronous
-			 ReadFileInitiate('../data/data01.js');
-		});
-	}
+    }
+    );
+    document.onkeydown = HandleKeyDown;
+    $(document).ready(function() {
+      //generate all problem data for main page -- this is asynchronous
+       ReadFileInitiate('../data/data01.js');
+    });
+  }
 )
