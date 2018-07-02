6382fe829366bfcfb8b69567c6ed97bbd15f7bf1
Artur Fijał <afijal@soldevelo.com>
2018-07-02 18:07:30 +0200

    #207: Add react related files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
