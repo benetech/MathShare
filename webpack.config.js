6fb9a1d98ba718190ceb77ce9f174b29f1e255e3
Artur Fijał <afijal@soldevelo.com>
2018-07-03 15:17:01 +0200

    207: Added modular css support

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,21 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
