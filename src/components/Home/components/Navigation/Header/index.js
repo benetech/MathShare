2672620778b9f1ddbc25788b60f19b674b04b7e7
Artur Fijał <afijal@soldevelo.com>
2018-07-05 14:00:56 +0200

    #241: Transition of problem chooser header to react component

---
+++ src/components/Home/components/Navigation/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classNames from "classnames";
+import styles from '../../../../../styles/styles.css';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class MainPageFooter extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2 id="LeftNavigationHeader" className={styles.sROnly}> Problems to solve </h2>
+                <div class="row">
+                    <div className={
+                        classNames({
+                            [bootstrap['col-lg-12']]: true,
+                            [bootstrap['text-center']]: true
+                        })
+                    }>
+                        <h2>Select a problem and try out Benetech's Math Editor (alpha)!</h2>
+                        <br/><br/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
