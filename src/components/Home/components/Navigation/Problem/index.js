4c366bac9929b44e73de9e12baf82ad9161f66c5
oskarhinc <ohinc@soldevelo.com>
2018-07-10 10:44:44 +0200

    #254: Transited button to a seperate React component

--- src/components/Home/components/Navigation/Problem/index.js
+++ src/components/Home/components/Navigation/Problem/index.js
@@ -1,19 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import Button from '../../.././../Button';
 import classNames from "classnames";
 import problem from './styles.css';
-import styles from '../../../../../styles/styles.css';
+import buttons from '../../../../../components/Button/styles.css';
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Problem extends Component {
     render() {
         return (
-            <li className={classNames(bootstrap['col-md-4'], bootstrap['text-center'], problem.problem)}>
-                <span className={classNames(bootstrap.btn, styles['btn-default'], styles['btn-huge'])}>
-                    <button className={classNames(problem.navItemButton, problem.colorInherit)}>
-                         <span className={problem.problemAnnotation}> {this.props.annotation} </span>
-                    </button>
-                    <span className={problem.problemEquation}> {this.props.equation} </span>
-                </span> 
+            <li
+                className={
+                    classNames(
+                        bootstrap['col-md-4'],
+                        bootstrap['text-center'],
+                        problem.problem
+                    )
+                }
+            >
+                <span
+                    className={
+                        classNames(
+                            bootstrap.btn,
+                            buttons.default,
+                            buttons.huge
+                        )
+                    }
+                >
+
+                    <Button
+                        className={
+                            classNames(
+                                problem.navItemButton,
+                                problem.colorInherit
+                            )
+                        }
+                        content={<span className={problem.problemAnnotation}>{this.props.annotation}</span>}
+                    />
+                    <span className={problem.problemEquation}>{this.props.equation}</span>
+                </span>
             </li>
         );
     }
