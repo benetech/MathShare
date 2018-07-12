f31ffccfe16487c5aac49ea4f2783379d1f6fe79
Artur Fijał <afijal@soldevelo.com>
2018-07-12 17:22:20 +0200

    Switch to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './components';
 import 'jquery/dist/jquery';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import dataSet from './data/data01.json';
 
 render((
-  <BrowserRouter>
+  <HashRouter>
     <App dataSet={dataSet} />
-  </BrowserRouter>
+  </HashRouter>
 ), document.getElementById('root'));
