d1270a7d6fa3c729fd504110f293459278fd7929
Artur Fijał <afijal@soldevelo.com>
2018-08-29 16:33:37 +0200

    #395: Renumber problems from 1 not 0 when adding new

--- src/components/Home/components/NewProblemsForm/index.js
+++ src/components/Home/components/NewProblemsForm/index.js
@@ -42,7 +42,7 @@ export default class NewProblemsForm extends Component {
             
             return <div className={styles.row}>
                 <div className={styles.ordinal}>
-                        {i}.
+                        {i + 1}.
                 </div>
                 <div className={styles.cell}>
                     {'$$' + problem.text + '$$'}
