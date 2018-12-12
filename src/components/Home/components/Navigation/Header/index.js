0abaf7e90249f176169eaab75591f6e527b14406
Neil Soiffer <soiffer@alum.mit.edu>
2018-12-11 22:49:43 -0800

    Removed extranious <h2> and changed an <h2> to <h1> since it is the start of the page's main content.

--- src/components/Home/components/Navigation/Header/index.js
+++ src/components/Home/components/Navigation/Header/index.js
@@ -3,14 +3,11 @@ import classNames from 'classnames';
 import Locales from '../../../../../strings';
 
 const NavigationHeader = () => (
-    <div>
-        <h2 id="LeftNavigationHeader" className="sROnly">{Locales.strings.problems_to_solve}</h2>
-        <div className="row">
-            <div className={classNames('col-lg-12', 'text-center')}>
-                <h2>{Locales.strings.select_a_problem}</h2>
-                <br />
-                <br />
-            </div>
+    <div className="row">
+        <div className={classNames('col-lg-12', 'text-center')}>
+            <h1 id="LeftNavigationHeader">{Locales.strings.select_a_problem}</h1>
+            <br />
+            <br />
         </div>
     </div>
 );
