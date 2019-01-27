44442037793dc9c8bba73c744eb4714bd2cc3b35
NSoiffer <soiffer@alum.mit.edu>
2019-01-27 22:57:56 +0000

    Accessibility fix for <main> heading so it makes sense if you jump to it.

--- src/components/Home/components/Navigation/Header/index.js
+++ src/components/Home/components/Navigation/Header/index.js
@@ -5,11 +5,10 @@ import Locales from '../../../../../strings';
 const NavigationHeader = () => (
     <div className="row">
         <div className={classNames('col-lg-12', 'text-center')}>
-            <h1 id="LeftNavigationHeader" tabIndex="-1">XXX MathShare beta</h1>
+            <h1 id="LeftNavigationHeader" tabIndex="-1">{Locales.strings.select_a_problem_header}</h1>
             <p>{Locales.strings.select_a_problem}</p>
             <br aria-hidden="true" />
             <br aria-hidden="true" />
-            <h2 className={classNames('sr-only')}> XXX Problems</h2>
         </div>
     </div>
 );
