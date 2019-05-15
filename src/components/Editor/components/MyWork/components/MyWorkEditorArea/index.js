11af2f4f9e9e4f26924aee06727ec75df59601e2
NSoiffer <soiffer@alum.mit.edu>
2019-05-15 02:24:20 +0100

    Turn on smartMode. Also add a few shortcuts.
    TODO: Should adjust "text" palette to toggle between text and math.

--- src/components/Editor/components/MyWork/components/MyWorkEditorArea/index.js
+++ src/components/Editor/components/MyWork/components/MyWorkEditorArea/index.js
@@ -21,6 +21,8 @@ export default class MyWorkEditorArea extends Component {
             {
                 commandbarToggle: 'hidden',
                 virtualKeyboardMode: 'off',
+                smartMode: true,
+                smartFence: false,
                 overrideDefaultInlineShortcuts: false,
                 inlineShortcuts:
                 {
@@ -32,13 +34,15 @@ export default class MyWorkEditorArea extends Component {
                     '$': '\\$', // make it easy to type $
                     '%': '\\%', // make it easy to type %
                     '*': '\\times', // what most people want
-                    '?=': '\\overset{?}{=}',	// is equal to
-                    cbrt: '\\sqrt[{3}]{#0}',	// cube root,
-                    root: '\\sqrt[{#?}]{#0}',	// general root
+                    '?=': '\\overset{?}{=}', // is equal to
+                    sqrt: '\\sqrt{#0}', // square root (currently broken in mathlive)
+                    squareroot: '\\sqrt{#0}', // square root (for consistency with 'cuberoot')
+                    cbrt: '\\sqrt[3]{#0}', // cube root
+                    cuberoot: '\\sqrt[3]{#0}', // cube root
+                    root: '\\sqrt[{#?}]{#0}', // general root
                 },
                 inlineShortcutBackspaceCommand: 'delete', // make deleting multichar shortcut be atomic
                 removeExtraneousParentheses: false, // if typed, keep parens around
-                smartFence: false,
                 onMoveOutOf: () => false,
                 onKeystroke: (key) => {
                     if (key === 'Enter') {
