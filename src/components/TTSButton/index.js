24403534b47361930857757a1b89c1b99e98d6f1
Rupesh Parab <rupeshparab.rp@gmail.com>
2020-06-29 22:59:17 +0530

    #1000 - move to redux state machine and add third loading state

--- src/components/TTSButton/index.js
+++ src/components/TTSButton/index.js
@@ -7,69 +7,25 @@ import styles from './styles.scss';
 import Locales from '../../strings';
 import uiActions from '../../redux/ui/actions';
 import ariaLiveActions from '../../redux/ariaLiveAnnouncer/actions';
-import { stopEvent } from '../../services/events';
-import { alertError } from '../../scripts/alert';
+import ttsActions from '../../redux/tts/actions';
+import { initializePollyClient } from '../../services/aws';
 
 class TTSButton extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            ttsState: null,
-            text: this.getText(props.text),
-            audio: null,
-            ttsConfig: null,
-        };
-        if (props.userProfile.config) {
-            this.state.ttsConfig = props.userProfile.config.tts || {
-                speed: 1.0,
-            };
-        }
         this.polly = new window.AWS.Polly({ apiVersion: '2016-06-10' });
         this.ttsId = `${props.id}-${Math.round(Math.random() * 1000)}`;
         this.debouncedClick = lodash.debounce(this.clickTtsBtn, 250, {
             leading: true,
             trailing: true,
         });
-    }
-
-    componentDidMount() {
-        this.generateUrl();
-    }
-
-    componentWillReceiveProps(newProps) {
-        if (newProps.ui.ttsId !== this.ttsId) {
-            if (this.state.ttsState) {
-                const audio = this.state.audio;
-                if (audio) {
-                    audio.pause();
-                    audio.currentTime = 0;
-                    this.setState({ ttsState: false });
-                }
-            }
-        } else if (this.state.text !== this.getText(newProps.text)
-        || (
-            newProps.userProfile.config
-            && this.compareTtsConfig(newProps)
-        )) {
-            this.clearListeners(true);
-            this.setState({
-                tsState: null,
-                text: this.getText(newProps.text),
-                audio: null,
-                ttsConfig: (newProps.userProfile.config || {}).tts || {},
-            }, () => {
-                this.generateUrl(true, 0);
-            });
-        }
+        this.props.loadTtsAudio(this.ttsId, this.getText(props.text));
+        initializePollyClient();
     }
 
     componentWillUnmount() {
-        this.clearListeners();
+        this.props.dismountTtsButton(this.ttsId);
     }
-
-    compareTtsConfig = newProps => (
-        JSON.stringify(newProps.userProfile.config.tts) !== JSON.stringify(this.state.ttsConfig)
-    )
 
     getText = (inputText) => {
         if (typeof inputText === 'function') {
@@ -78,101 +34,24 @@ class TTSButton extends Component {
         return inputText;
     }
 
-    playAudio = (retryNo) => {
-        const { audio } = this.state;
-        audio.play()
-            .then(() => {
-                this.props.setTtsPlaying(this.ttsId);
-            })
-            .catch(() => {
-                if (retryNo < 1) {
-                    this.generateUrl(true, retryNo + 1);
-                } else {
-                    alertError(Locales.strings.tts_error, Locales.strings.error);
-                    this.props.clearAriaLive();
-                    this.props.announceOnAriaLive(Locales.strings.tts_error);
-                    this.endAudio();
-                }
-            });
-    }
-
-    endAudio = () => {
-        this.setState({ ttsState: false });
-        this.props.stopTtsPlaying(this.ttsId);
-    }
-
-    clearListeners = (dontStop) => {
-        const audio = this.state.audio;
-        if (audio) {
-            audio.pause();
-            audio.currentTime = 0;
-            audio.removeEventListener('ended', this.endAudio);
-            if (!dontStop) {
-                this.endAudio();
-                this.props.stopTtsPlaying(this.ttsId);
-            }
+    clickTtsBtn = () => {
+        const { currentTtsId } = this.props.tts;
+        if (currentTtsId === this.ttsId) {
+            this.props.stopTtsAudio(this.ttsId);
+        } else {
+            this.props.startTts(this.ttsId, this.getText(this.props.text));
         }
-    }
-
-    clickTtsBtn = (e) => {
-        this.setState((prevState) => {
-            const audio = prevState.audio;
-            if (prevState.ttsState) {
-                audio.pause();
-                audio.currentTime = 0;
-                this.props.stopTtsPlaying(this.ttsId);
-            } else {
-                this.playAudio(0);
-            }
-            return { ttsState: !prevState.ttsState };
-        });
-        return stopEvent(e);
-    }
-
-    getSsml = () => {
-        const text = this.state.text;
-        const { ttsConfig } = this.state;
-        let speed = 100;
-        if (ttsConfig && ttsConfig.speed > 0) {
-            speed *= ttsConfig.speed;
-        }
-        return `<speak><prosody rate="${Math.round(speed)}%">${text}</prosody></speak>`;
-    }
-
-    generateUrl = (playOnGeneration, retryNo) => {
-        if (this.state.text === null) {
-            return;
-        }
-        const speechParams = {
-            OutputFormat: 'mp3',
-            SampleRate: '16000',
-            Text: this.getSsml(),
-            TextType: 'ssml',
-            VoiceId: 'Matthew',
-        };
-        const signer = new window.AWS.Polly.Presigner(speechParams, this.polly);
-        signer.getSynthesizeSpeechUrl(speechParams, (error, url) => {
-            if (!error) {
-                const audio = new Audio(url);
-                audio.load();
-                audio.addEventListener('ended', this.endAudio);
-                this.setState({
-                    ttsState: playOnGeneration,
-                    audio,
-                }, () => {
-                    if (playOnGeneration) {
-                        this.playAudio(retryNo || 0);
-                    }
-                });
-            }
-        });
     }
 
     getAriaLabel = () => {
-        const { ttsState } = this.state;
+        const { currentTtsId, currentTtsStatus } = this.props.tts;
         let prefix = '';
-        if (ttsState) {
-            prefix = Locales.strings.stop_speaking;
+        if (currentTtsId === this.ttsId) {
+            if (currentTtsStatus) {
+                prefix = Locales.strings.stop_speaking;
+            } else {
+                prefix = Locales.strings.loading_2;
+            }
         } else {
             prefix = Locales.strings.speak;
         }
@@ -180,17 +59,27 @@ class TTSButton extends Component {
     }
 
     render() {
-        const { ttsState } = this.state;
-        if (ttsState === null) {
-            return <span className={this.props.spanStyle || ''} />;
-        }
+        const { currentTtsId, currentTtsStatus } = this.props.tts;
         const tooltip = (
             <UncontrolledTooltip placement="top" target={this.props.id || this.ttsId}>
                 {this.props.title}
             </UncontrolledTooltip>
         );
 
-        const iconName = (ttsState ? 'stop-circle' : 'volume-up');
+        let iconName = '';
+        let iconClassName = '';
+        let spin = false;
+        if (currentTtsId === this.ttsId) {
+            if (currentTtsStatus) {
+                iconName = 'stop-circle';
+            } else {
+                iconName = 'circle-o-notch';
+                iconClassName = styles.smallIcon;
+                spin = true;
+            }
+        } else {
+            iconName = 'volume-up';
+        }
 
         const button = (
             <button
@@ -200,9 +89,8 @@ class TTSButton extends Component {
                 type="button"
                 onClick={this.debouncedClick}
             >
-                <FontAwesome name={iconName} size="lg" />
+                <FontAwesome className={iconClassName} name={iconName} size="lg" spin={spin} />
             </button>
-
         );
 
         return (
@@ -219,9 +107,11 @@ export default connect(
     state => ({
         ui: state.ui,
         userProfile: state.userProfile,
+        tts: state.tts,
     }),
     {
         ...uiActions,
         ...ariaLiveActions,
+        ...ttsActions,
     },
 )(TTSButton);
